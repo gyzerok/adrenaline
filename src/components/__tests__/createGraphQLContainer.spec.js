5121ad6f11933f5b87158147ed6b38476502a3a0
Fedor Nezhivoy <gyzerok@gmail.com>
2015-08-06 13:13:33 +0600

    added dummy test for createGraphQLContainer

---
+++ src/components/__tests__/createGraphQLContainer.spec.js
@@ -0,0 +1,17 @@
+jest.autoMockOff();
+
+describe('createGraphQLContainer', () => {
+  it('should return React Component', () => {
+    /*const React = require('react');
+    const createGraphQLDecorator = require('../createGraphQLDecorator');
+
+    @createGraphQLDecorator(x => x)
+    class TestComponent {
+      render() {
+        return <div></div>;
+      }
+    }*/
+
+    expect(true).toBe(true);
+  });
+});
