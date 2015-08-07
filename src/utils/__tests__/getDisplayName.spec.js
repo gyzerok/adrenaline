55669d95e4712cff5643d8fb8c41b420f1fbbbc4
Fedor Nezhivoy <gyzerok@gmail.com>
2015-08-07 10:19:24 +0600

    fixed getDisplayName test

--- src/utils/__tests__/getDisplayName.spec.js
+++ src/utils/__tests__/getDisplayName.spec.js
@@ -1,8 +1,14 @@
 import test from 'tape';
 import getDisplayName from '../getDisplayName';
 
-test('getDisplayName should return Component for empty object', assert => {
-  assert.equal(getDisplayName({}), 'Component');
+test('getDisplayName should return String or Component for empty object', assert => {
+  const mapped = [
+    { displayName: 'hey' },
+    { name: 'ho' },
+    {}
+  ].map(getDisplayName);
+
+  assert.deepEqual(mapped, ['hey', 'ho', 'Component']);
 
   assert.end();
 });
