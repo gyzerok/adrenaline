46b036ec10d8d3ddcd0a4b00dd280bd4f7a7cf13
Fedor Nezhivoy <gyzerok@gmail.com>
2015-08-27 10:01:53 +0600

    using new normalize implementation

--- src/reducers/graphql.js
+++ src/reducers/graphql.js
@@ -1,36 +1,9 @@
 /* @flow */
 
-import { map, reduce } from 'lodash';
-import mergeDeep from 'deepmerge';
+import normalize from '../utils/normalize';
 import { ACTION_TYPE } from '../constants';
 
-export default function deserialize(initial, data = {}) {
-  return reduce(data, (acc, node) => {
-    const { edges, ...stuff } = node;
-
-    if (!edges) {
-      return node;
-    }
-
-    const normalizedNode = {
-      ...stuff,
-      ...reduce(edges, (memo, value, key) => {
-        if (Array.isArray(value)) {
-          return { ...memo, [key]: map(value, x => x.id) };
-        }
-        return { ...memo, [key]: value.id };
-      }, {}),
-    };
-    const normalizedEdges = deserialize({}, edges);
-
-    return mergeDeep(acc, {
-      [normalizedNode.id]: normalizedNode,
-      ...normalizedEdges,
-    });
-  }, initial);
-}
-
-export default function graphql(state = {}, action, normalize = deserialize) {
+export default function graphql(state = {}, action, deserialize = normalize) {
   const { type, payload, error } = action;
 
   if (error) {
@@ -39,9 +12,7 @@ export default function graphql(state = {}, action, normalize = deserialize) {
 
   switch (type) {
     case ACTION_TYPE:
-      const nextState = normalize(state, payload);
-      console.log('state', nextState);
-      return nextState;
+      return deserialize(state, payload);
     default:
       return state;
   }
