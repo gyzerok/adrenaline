1d23603ea0ab8e337b385f5bb60350f0f603fcb0
Fedor Nezhivoy <gyzerok@gmail.com>
2015-08-28 20:11:59 +0600

    fixed normalize

--- src/utils/normalize.js
+++ src/utils/normalize.js
@@ -5,34 +5,36 @@ import merge from './merge';
 
 export default function normalize(initial, data) {
   return reduce(data, (acc, node) => {
-    const { edges, ...stuff } = node;
-
     if (Array.isArray(node)) {
       return merge(acc, {
-        ...reduce(node, (memo, value) => {
-          return { ...memo, [value.id]: value };
-        }, {}),
+        ...reduce(node, normalizeOne, initial),
       });
     }
 
-    if (!edges) {
-      return merge(acc, { [node.id]: node });
-    }
-
-    const normalizedNode = {
-      ...stuff,
-      ...reduce(edges, (memo, value, key) => {
-        if (Array.isArray(value)) {
-          return { ...memo, [key]: map(value, x => x.id) };
-        }
-        return { ...memo, [key]: value.id };
-      }, {}),
-    };
-    const normalizedEdges = normalize({}, edges);
-
-    return merge(acc, {
-      [node.id]: normalizedNode,
-      ...normalizedEdges,
-    });
+    return normalizeOne(initial, node);
   }, initial);
+}
+
+function normalizeOne(initial, node) {
+  const { edges, ...stuff } = node;
+
+  if (!edges) {
+    return merge(initial, { [node.id]: node });
+  }
+
+  const normalizedNode = {
+    ...stuff,
+    ...reduce(edges, (memo, value, key) => {
+      if (Array.isArray(value)) {
+        return { ...memo, [key]: map(value, x => x.id) };
+      }
+      return { ...memo, [key]: value.id };
+    }, {}),
+  };
+  const normalizedEdges = normalize({}, edges);
+
+  return merge(initial, {
+    [node.id]: normalizedNode,
+    ...normalizedEdges,
+  });
 }
