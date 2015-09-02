37d5d1ad2e1d120506dd290675ce99c53920a4f6
Fedor Nezhivoy <gyzerok@gmail.com>
2015-09-02 13:35:44 +0600

    new possible mutation API

--- example/src/client/mutations/todo.js
+++ example/src/client/mutations/todo.js
@@ -10,6 +10,25 @@ export const createTodo = (text) => `
   }
 `;
 
+export const test = {
+  mutation: ({ text }) => `
+    mutation AppMutation {
+      createTodo(text: "${text}") {
+        id,
+        text,
+        createdAt
+      }
+    }
+  `,
+  collisionKey: () => 'hello',
+  resolve: (todo) => ({
+    type: 'ADD',
+    strategy: 'APPEND',
+    id: todo.id,
+    parentId: todo.user.id,
+  }),
+};
+
 export const removeTodo = (id) => `
   mutation AppMutation {
     removeTodo(id: ${id}) {
