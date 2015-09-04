f5e0757c790fdbba39b6673a971da9672bf19189
Fedor Nezhivoy <gyzerok@gmail.com>
2015-09-04 17:50:43 +0600

    requests are now all post

--- src/network/request.js
+++ src/network/request.js
@@ -1,25 +1,32 @@
 /* @flow */
 
 export default function request(endpoint, data, files) {
-  if (data.query) {
-    const query = encodeURIComponent(data.query);
-    const variables = encodeURIComponent(JSON.stringify(data.params));
-    return fetch(`${endpoint}?query=${query}&variables=${variables}`).then(parseJSON);
-  }
-  if (data.mutation) {
-    const formData = new FormData();
-    formData.append('query', data.mutation);
-    formData.append('variables', JSON.stringify(data.params));
-    if (files) {
-      for(let i = 0; i < files.length; i++) {
-        formData.append('files', files[i], files[i].name);
-      }
-    }
+  if (!files) {
     return fetch(endpoint, {
       method: 'post',
-      body: formData,
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: data.query,
+        variables: data.params,
+      }),
     }).then(parseJSON);
   }
+
+  const formData = new FormData();
+  formData.append('query', data.mutation);
+  formData.append('variables', JSON.stringify(data.params));
+  if (files) {
+    for(let i = 0; i < files.length; i++) {
+      formData.append('files', files[i], files[i].name);
+    }
+  }
+  return fetch(endpoint, {
+    method: 'post',
+    body: formData,
+  }).then(parseJSON);
 
   throw new Error('Unsupported request');
 }
