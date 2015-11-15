204fb60f2727880cf25a9770445024ed2ad30a51
Fedor Nezhivoy <gyzerok@gmail.com>
2015-11-15 17:15:26 +0600

    fix names again

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@
 export Adrenaline from './components/Adrenaline';
 export createContainer from './components/createContainer';
 export createDumbComponent from './components/createDumbComponent';
-export createAdaptor from './adaptors/graphql';
+export createGraphQLAdaptor from './adaptors/graphql';
