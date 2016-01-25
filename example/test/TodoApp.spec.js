db8b75c48d2dc9a54fa912080ca14b040e940132
Fedor Nezhivoy <gyzerok@gmail.com>
2016-01-25 12:25:35 +0600

    expect test utils

--- example/test/TodoApp.spec.js
+++ example/test/TodoApp.spec.js
@@ -1,23 +1,15 @@
 import expect from 'expect';
 import { parse } from 'graphql/language';
 import { validate } from 'graphql/validation';
+import adrenalineTestUtils from 'adrenaline/graphql/test-utils/expect';
 
 import schema from '../src/shared/schema';
 import TodoApp from '../src/client/graphql/components/TodoApp';
 
+expect.extend(adrenalineTestUtils);
+
 describe('Queries regression', () => {
-  it('for TodoApp', (done) => {
-    const queries = TodoApp.getQueries()();
-    const combinedQueries = Object
-      .keys(queries)
-      .reduce((acc, key) => {
-        return `${acc} ${queries[key]}`;
-      }, '');
-    const graphQLQuery = `query TestQuery { ${combinedQueries} }`;
-
-    const errors = validate(schema, parse(graphQLQuery));
-    if (errors.length === 0) return done();
-
-    throw errors[0];
+  it('for TodoApp', () => {
+    expect(TodoApp).toBeValidAgainst(schema);
   });
 });
